0a4375e9d5415311ff3a16b47660f9b3bae24bc1
Tschipp <mrtschipp@gmail.com>
2020-05-09 15:40:40 +0200

    Initial Release

---
+++ src/main/java/tschipp/bbprojecte/BBProjectE.java
@@ -0,0 +1,44 @@
+package tschipp.bbprojecte;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import tschipp.buildersbag.api.ModuleRegistry;
+
+@EventBusSubscriber
+@Mod(modid = BBProjectE.MODID, name = BBProjectE.NAME, version = BBProjectE.VERSION, dependencies = BBProjectE.DEPENDENCIES, acceptedMinecraftVersions = BBProjectE.ACCEPTED_VERSIONS, guiFactory = "tschipp.buildersbag.client.gui.GuiFactoryBuildersBag", certificateFingerprint = BBProjectE.CERTIFICATE)
+public class BBProjectE
+{
+	public static final String MODID = "bbprojecte";
+	public static final String VERSION = "GRADLE:VERSION";
+	public static final String NAME = "Builder's Bag - ProjectE Addon";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2,1.13)";
+	public static final String DEPENDENCIES = "required-after:forge@[13.20.1.2386,);required-after:projecte;required-after:buildersbag@[1.0.1.6,);";
+	public static final Logger LOGGER = LogManager.getFormatterLogger(MODID.toUpperCase());
+	public static final String CERTIFICATE = "fd21553434f4905f2f73ea7838147ac4ea07bd88";
+
+	private static final String curseSlug = "buildersbag-projecte";
+	
+	public static boolean FINGERPRINT_VIOLATED = false;
+
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		ModuleRegistry.registerModule(MODID, new ResourceLocation(MODID, "projecte"), ProjectEBagModule::new, 5);
+	}
+
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent event)
+	{
+		LOGGER.error("WARNING! Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with! If you didn't download the file from https://minecraft.curseforge.com/projects/" + curseSlug + " or through any kind of mod launcher, immediately delete the file and re-download it from https://minecraft.curseforge.com/projects/" + curseSlug);
+		FINGERPRINT_VIOLATED = true;
+	}
+}
---
+++ src/main/java/tschipp/bbprojecte/ProjectEBagModule.java
@@ -0,0 +1,82 @@
+package tschipp.bbprojecte;
+
+import moze_intel.projecte.api.item.IItemEmc;
+import moze_intel.projecte.gameObjs.ObjHandler;
+import moze_intel.projecte.utils.EMCHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.items.ItemStackHandler;
+import tschipp.buildersbag.api.AbstractBagModule;
+import tschipp.buildersbag.api.IBagCap;
+import tschipp.buildersbag.common.inventory.ItemHandlerWithPredicate;
+
+public class ProjectEBagModule extends AbstractBagModule {
+
+	private ItemStackHandler handler = new ItemHandlerWithPredicate(1, (stack, slot) -> { return stack.getItem() instanceof IItemEmc;});
+	
+	private static final ItemStack DISPLAY = new ItemStack(ObjHandler.philosStone);
+	
+	public ProjectEBagModule() {
+		super(BBProjectE.MODID + ":projecte");
+	}
+
+	@Override
+	public NonNullList<ItemStack> getPossibleStacks(IBagCap bag) {
+		return NonNullList.create();
+	}
+
+	@Override
+	public ItemStack createStack(ItemStack stack, IBagCap bag, EntityPlayer player) {
+		
+		ItemStack storedItem = handler.getStackInSlot(0);
+		if(storedItem.isEmpty())
+			return ItemStack.EMPTY;
+		
+		long emcValue = EMCHelper.getEmcValue(stack);
+		if(emcValue == 0)
+			return ItemStack.EMPTY;
+		
+		IItemEmc emcHolder = (IItemEmc) storedItem.getItem();
+		long storedEmc = emcHolder.getStoredEmc(storedItem);
+		
+		if(emcValue > storedEmc)
+			return ItemStack.EMPTY;
+		
+		emcHolder.extractEmc(storedItem, emcValue);
+		
+		return stack.copy();
+	}
+
+	@Override
+	public NBTTagCompound serializeNBT()
+	{
+		NBTTagCompound tag = super.serializeNBT();
+		tag.setTag("Inventory", handler.serializeNBT());
+		return tag;
+	}
+
+	@Override
+	public void deserializeNBT(NBTTagCompound nbt)
+	{
+		super.deserializeNBT(nbt);
+		handler.deserializeNBT(nbt.getCompoundTag("Inventory"));
+	}
+	
+	@Override
+	public boolean doesntUseOwnInventory() {
+		return false;
+	}
+
+	@Override
+	public ItemStackHandler getInventory() {
+		return handler;
+	}
+
+	@Override
+	public ItemStack getDisplayItem() {
+		return DISPLAY;
+	}
+
+}
